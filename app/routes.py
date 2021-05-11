96602476872e8a60ac0fa4f94cc6754e94d6fd84
unknown <sumitpatil0420@gmail.com>
2021-05-11 14:36:36 +0530

    added home and login templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,26 @@
 
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username':'Nam'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+            {
+                'author': {'username': 'John'},
+                'body': 'Beutiful day in Portland!'
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+            }
+            ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
